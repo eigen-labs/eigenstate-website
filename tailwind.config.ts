1805870ad424bbb591e833080815fa6fac3e7370
Mario <mario@decentai.com>
2023-12-23 13:44:36 -0800

    added navbar for desktop and mobile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,13 +5,31 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      width: {
+        '1px': '1px',
+        '27px': '27px'
+      },
+      height: {
+        '1px': '1px',
+        '27px': '27px'
+      },
+      fontFamily: {
+        orbitron: ['Orbitron', 'sans-serif'] 
+      },
+      backgroundColor: {
+        'system-color': 'rgb(var(--background-color-rgb))',
+      },
+      rotate: {
+        '-45': '-45deg',
+        '45': '45deg',
+      },
+      margin: {
+        '9px': '9px',
       },
     },
   },
